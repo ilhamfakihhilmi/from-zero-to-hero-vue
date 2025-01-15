c3c2be201ede763e83516e5203c9cd5bee44133d
Fakih <ilhamfakih50@gmail.com>
2025-01-15 11:36:44 +0700

    Boolean Attribute

--- belajar-vuejs-dasar/src/components/HelloVue.vue
+++ belajar-vuejs-dasar/src/components/HelloVue.vue
@@ -4,11 +4,14 @@ console.log("Hello Vue");
 const hello = "<h1> Hello Vue </h1>";
 const classHello = "hello";
 const id = "hello";
+const buttonDisables = true;
 </script>
 
 <template>
   <h1 :id :class="classHello">{{ hello }}</h1>
   <div :class="classHello" v-html="hello"></div>
+
+  <button :disabled="buttonDisables">This is Button</button>
 </template>
 
 <style scoped>
